fd2bd3cc7ee4583155643863aab0939dc73edcc9
Federico Casadei <116793299+casaddei@users.noreply.github.com>
2022-12-16 09:31:52 +0100

    Add files via upload

---
+++ Alfabeto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+// write in 2019 by maurizio.conti@fablabromagna.org
+
+public static class Alfabeto
+{
+    public static bool Verifica(string input)
+    {
+        string alfabeto = "abcdefghijklmnopqrstuvwxyz";
+
+        input.ToLower();
+        int dimensione = input.Length;
+        int vero = 0;
+        for (int i = 0; i < dimensione; i++)
+        {
+            for (int j = 0; j < 26; j++)
+            {
+                if (input[i] == alfabeto[j])
+                {
+                    vero=vero++;
+                }
+                else
+                {
+                    vero = 0;
+                }
+            }
+        }
+        if(vero>=26)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
